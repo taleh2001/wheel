787cf44602bbe9754b88ef13ed10eac5f1b23f0b
taleh2001 <58920280+taleh2001@users.noreply.github.com>
2023-05-18 20:45:04 +0300

    Delete index.js

--- js/index.js
+++
@@ -1,6 +0,0 @@
-
-let burger = document.querySelector(".burger")
-let header = document.querySelector('.header')
-burger.addEventListener('click', function(){
-    header.classList.toggle('open')
-})
